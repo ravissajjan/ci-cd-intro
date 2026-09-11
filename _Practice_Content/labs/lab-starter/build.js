1273138d431e9c6a76a270772a3d253ccc68fe0d
ravissajjan <ravissajjan@gmail.com>
2026-09-11 10:32:41 +0000

    Update Repo

--- _Practice_Content/labs/lab-starter/build.js
+++
@@ -1,9 +0,0 @@
-// The "build" step: put what we want to ship into a dist folder.
-
-const fs = require('fs');
-
-fs.rmSync('dist', { recursive: true, force: true });   // always start from a clean folder
-fs.mkdirSync('dist', { recursive: true });
-fs.copyFileSync('index.html', 'dist/index.html');
-
-console.log('Build done -> dist/index.html');
